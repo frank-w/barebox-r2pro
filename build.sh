cd3c35655c966454a5e6d7c8fcbea6ae32802b74
Frank Wunderlich <frank-w@public-files.de>
2022-01-05 16:36:52 +0100

    build.sh: add basic build script and add own defconfig with menu support

---
+++ build.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+export ARCH=arm64
+export CROSS_COMPILE=aarch64-linux-gnu-
+
+case $1 in
+"importconfig")
+	make rk3568_defconfig
+	;;
+"defconfig")
+	nano arch/arm/configs/rk3568_defconfig
+	;;
+"config")
+	make menuconfig
+	;;
+"build")
+	make
+	;;
+"board")
+	nano arch/arm/boards/rockchip-rk3568-bpi-r2pro/board.c
+	;;
+"dts")
+	nano arch/arm/dts/rk3568-bpi-r2-pro.dts
+	;;
+"install")
+	dev=/dev/sdb
+	choice=y
+	read -e -i "$dev" -p "Please enter target device: " dev
+	if [[ $? -eq 0 ]];then
+		sudo dd if=images/barebox-rk3568-r2pro.img of=$dev bs=1024 seek=32
+	fi
+	;;
+*)
+	echo "unknown command $1"
+	;;
+esac
