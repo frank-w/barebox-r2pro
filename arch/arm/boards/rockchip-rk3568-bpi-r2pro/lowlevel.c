6cafcdaeb67d6ecbf4b9f0ab982360a56ba0e424
Frank Wunderlich <frank-w@public-files.de>
2022-05-13 14:23:41 +0200

    ARM: dts: set iodomain right for v00 too (vccio4 and vccio6 swapped)

--- arch/arm/boards/rockchip-rk3568-bpi-r2pro/lowlevel.c
+++ arch/arm/boards/rockchip-rk3568-bpi-r2pro/lowlevel.c
@@ -16,15 +16,6 @@ static noinline void rk3568_start(void *fdt_blob_fixed_offset)
 {
 	void *fdt;
 
-	/*
-	 * set iodomain vccio6 to 1.8V needed for GMAC1 to work.
-	 * vccio4 (gmac0/switch) needs to stay at 3v3 (default)
-	 */
-	//set bit 6 in PMU_GRF_IO_VSEL0 for vccio6 1v8
-	writel(RK_SETBITS(BIT(6)), PMU_GRF_IO_VSEL0);
-	//clear bit 6 for 3v3 as it was set to 1v8
-	writel(RK_CLRBITS(BIT(6)), PMU_GRF_IO_VSEL1);
-
 	fdt = fdt_blob_fixed_offset;
 
 	if (current_el() == 3) {
@@ -54,10 +45,28 @@ static void start_rk3568_bpi_r2pro_common(void *fdt_blob_fixed_offset)
 
 ENTRY_FUNCTION(start_rk3568_bpi_r2pro, r0, r1, r2)
 {
+	/*
+	 * set iodomain vccio6 to 1.8V needed for GMAC1 to work.
+	 * vccio4 (gmac0/switch) needs to stay at 3v3 (default)
+	 */
+	//set bit 6 in PMU_GRF_IO_VSEL0 for vccio6 1v8
+	writel(RK_SETBITS(BIT(6)), PMU_GRF_IO_VSEL0);
+	//clear bit 6 for 3v3 as it was set to 1v8
+	writel(RK_CLRBITS(BIT(6)), PMU_GRF_IO_VSEL1);
+
 	start_rk3568_bpi_r2pro_common(__dtb_rk3568_bpi_r2_pro_start);
 }
 
 ENTRY_FUNCTION(start_rk3568_bpi_r2pro_v00, r0, r1, r2)
 {
+	/*
+	 * set iodomain vccio4 to 1.8V needed for GMAC0 to work.
+	 * vccio6 (gmac1/switch) needs to stay at 3v3 (default)
+	 */
+	//set bit 4 in PMU_GRF_IO_VSEL0 for vccio4 1v8
+	writel(RK_SETBITS(BIT(4)), PMU_GRF_IO_VSEL0);
+	//clear bit 4 for 3v3 as it was set to 1v8
+	writel(RK_CLRBITS(BIT(4)), PMU_GRF_IO_VSEL1);
+
 	start_rk3568_bpi_r2pro_common(__dtb_rk3568_bpi_r2_pro_v00_start);
 }
